fa1d26075a449fa357119149ed9139da23db9da8
RizkyRamadhan <rama.yuhu@gmail.com>
2014-08-17 23:08:29 +0700

    remove mpn

--- mpn-Client/build/generated-sources/jax-ws/id/go/customs/mpn/client/cukai/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://bean.cukai.billing.customs.go.id/")
-package id.go.customs.mpn.client.cukai;
--- mpn-Client/build/generated-sources/jax-ws/id/go/customs/mpn/client/publik/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://service.mpn.customs.go.id/")
-package id.go.customs.mpn.client.publik;
